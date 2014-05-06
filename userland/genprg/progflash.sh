bd38c6037f33d0e19d9a7b631dc338c2ae2f3503
fisted <van.fisted@googlemail.com>
2014-05-06 04:48:36 +0200

    high-level script to program the flash works now

--- userland/genprg/progflash.sh
+++ userland/genprg/progflash.sh
@@ -1,32 +1,103 @@
 #!/bin/sh
 
+unset device
+unset baud
+
 set -e
 #set -x
 
-opts="h"
+opts="hd:b:D"
 usage() {
-	echo "usage: $0 [$opts] <address> <flash_word_size> <words_per_flash_page> <erase_page (0 or 1)>" >&2
-	    exit 1
+	echo "usage: $0 [-$opts] <blob>" >&2
+	exit 1
 }
 
-addr=0
+sercommd=0
 
 while getopts "$opts" i; do
 	case $i in
+	d) device="$OPTARG" ;;
+	b) baud="$OPTARG" ;;
+	D) sercommd=1 ;;
 	h|\\?) usage ;;
 	esac
 done
 
-if [ "$#" -ne 4 ]; then
+shift $((${OPTIND}-1))
+
+if [ "$#" -ne 1 ]; then
 	usage
 fi
 
-pagesz=$((${2}*${3}))
+immecmd="immectl"
+if [ $sercommd -ne 0 ]; then
+	immecmd="${immecmd} -D"
+fi
 
-./writexdata.sh -a 0xf000 -n $pagesz
-./mkprogroutine.sh $1 $2 $3 $4 | ./writexdata.sh $((0xf000+${pagesz}))
+if [ -n "$device" ]; then
+	immecmd="${immecmd} -d $device"
+fi
 
-echo "RUNINSTR -d 0x75 0xC7 0x51"
-./setpc.sh $((0xf000+${pagesz}))
-echo "RESUME"
-echo "WAITHALT"
+if [ -n "$baud" ]; then
+	immecmd="${immecmd} -b $baud"
+fi
+
+flashsz=32768
+pagesz=1024
+
+if [ "$(uname)" == "Linux" ]; then
+	sz=$(stat -c '%s' $1)
+else
+	sz=$(stat -f '%z' $1)
+fi
+
+if [ $sz -gt $flashsz ]; then
+	echo "that wouldn't fit..." >&2
+	exit 1
+fi
+
+pages=$(($sz/$pagesz))
+remain=$(($sz%$pagesz))
+
+if [ $remain -gt 0 ]; then
+	pages=$(($pages+1))
+fi
+
+tmp=$(mktemp /tmp/progflash.XXXXXX)
+
+echo "resetting master" >&2
+echo MSTRST | $immecmd
+sleep .2
+echo "reinit target" >&2
+echo REINIT | $immecmd
+sleep .2
+echo "init clock" >&2
+echo RUNINSTR -d 0x75 0xc6 0x00 | $immecmd
+sleep .2
+echo "waiting for proper status" >&2
+while [ "$(echo STATUS | $immecmd)" != "0xb2" ]; do
+	sleep .2
+done
+echo "erasing chip" >&2
+echo CHIPERASE | $immecmd
+sleep .2
+echo "waiting for proper status" >&2
+while [ "$(echo STATUS | $immecmd)" != "0xb2" ]; do
+	sleep .2
+done
+echo "chip erase done" >&2
+
+
+page=0
+while [ $page -lt $pages ]; do
+	head -c $pagesz </dev/zero | tr '\0' '\377' >$tmp
+	dd if="$1" of="$tmp" bs=$pagesz skip=$page count=1 conv=notrunc 2>/dev/null
+
+	echo "writing page $page" >&2
+	./writeflashpage.sh $page <$tmp | $immecmd
+
+	page=$(($page+1))
+done
+echo "finished..." >&2
+
+rm "$tmp"
